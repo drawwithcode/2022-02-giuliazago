3e8350a4ba7a04c32220853cc355d0f4bc4ba956
giuliazago <giulia.zago16@gmail.com>
2022-10-18 19:56:57 +0200

    Assignment2

--- sketch.js
+++ sketch.js
@@ -1,16 +1,81 @@
-function preload() {
-	// put preload code here
-}
+let square = []; // array of obj 
+let count = 500;
+let colorList = ['black','gray','darkblue'];
+let coltext = "white";
+b = 80;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	// put setup code here
-	const message = "This is a template reposotory\nfor the course elective Creative Coding\nCommunication Design, Politecnico di Milano";
-	textAlign(CENTER, CENTER);
-  textSize(16)
-	text(message, width/2, height/2);
+  createCanvas(windowWidth,windowHeight);
+  for(let i=0; i < count; i++) {
+    square.push(new Obj());
+  }
 }
 
 function draw() {
-	// put drawing code here
+	background(b);
+  for (let i = 0; i < count; i++) {
+    square[i].move();
+    square[i].squares();
+    square[i].shift();
+  }
+
+  // design a little square that follows the mouse
+  push();
+ 	fill("white");
+  	noStroke();
+  	rect(mouseX,mouseY, 15,15);
+  pop();
+
+  // write a message in the center of the canva
+  push();
+  	let msg = "Click the screen to change the color!";
+  	textFont("Source Code Pro");
+  	textStyle("bold");
+  	textAlign(CENTER);
+  	textSize(20);
+  	noStroke();
+  	fill(coltext);
+  	text(msg, width/2, height/1.5);
+  pop();
 }
+
+// obj class
+class Obj {
+  constructor() {
+    this.x = random(windowWidth);
+    this.y = random(windowHeight);
+  }
+
+  move() {
+    this.x += 4*random(-1,1)*noise(this.x);
+    this.y += 4*random(-1,1)*noise(this.y);
+    this.dimension = 25*noise(this.y);
+  }
+
+  // define the shape
+  squares() {
+    noFill();
+    strokeWeight(0.5);
+    stroke('white');
+    rect(this.x, this.y, this.dimension, this.dimension);
+  }
+  
+  shift() {
+  if(this.x - this.dimension > height) {
+    } 
+  }
+}
+
+// change color of the background with every press of the mouse
+  function mousePressed() {
+    b = random(colorList); 
+    if(coltext=== "white") {
+      coltext=colorList[2]; //change color of the text with a precise value of the array
+    }
+    else {colortext= "white"
+	}
+  }
+
+  function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+  }
